679f0d787aa7502915895c9716c4b2faebbc7389
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-03-29 21:00:34 +0300

    add boilerplate to login and register pages

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,22 +2,33 @@ import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
+
 def create_app(test_config=None):
-  # load .env file
-  load_dotenv()
+    # load .env file
+    load_dotenv()
 
-  # create and configure the app
-  app = Flask(__name__, instance_relative_config=True)
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
 
-  # ensure the instance folder exists
-  try:
-    os.makedirs(app.instance_path)
-  except OSError:
-    pass
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-  # a simple page that says hello
-  @app.route("/")
-  def index():
-    return render_template("home.html", debug=os.getenv("FLASK_DEBUG"))
+    # a simple page that says hello
+    @app.route("/")
+    def index():
+        return render_template("home.html", debug=os.getenv("FLASK_DEBUG"))
 
-  return app
+    # app login page
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
+
+    # app register page
+    @app.route("/register")
+    def register():
+        return render_template("register.html")
+
+    return app
