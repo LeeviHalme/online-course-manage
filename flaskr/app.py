64d31b9a6ce77a806499fbd91a423d9c133e90ec
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-04-02 19:35:12 +0300

    add course pages and move sample data to a module

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from dotenv import load_dotenv
+from sampleData import sampleCourses
 
 
 def create_app(test_config=None):
@@ -19,12 +20,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route("/")
     def index():
-        publicCourses = [
-            ("Example Course 1", "Example Description", "id1"),
-            ("Example Course 2", "Example Description", "id2"),
-            ("Example Course 3", "Example Description", "id3"),
-        ]
-        return render_template("home.html", courses=publicCourses)
+        return render_template("home.html", courses=sampleCourses)
 
     # app login page
     @app.route("/login")
@@ -35,5 +31,25 @@ def create_app(test_config=None):
     @app.route("/register")
     def register():
         return render_template("register.html")
+
+    # view all public courses page
+    @app.route("/courses")
+    def courses():
+        return render_template(
+            "courses.html", courses=sampleCourses, searchTerm="example search term"
+        )
+
+    # view course info page
+    @app.route("/courses/<course_id>")
+    def view_course(course_id):
+        ids = list(map(lambda c: c[3], sampleCourses))
+
+        # if id not found
+        if course_id not in ids:
+            return abort(404)
+
+        course = sampleCourses[ids.index(course_id)]
+
+        return render_template("view_course.html", course=course)
 
     return app
---
+++ flaskr/sampleData.py
@@ -0,0 +1,36 @@
+# create sample dataset
+# to preview rendering course content
+sampleCourses = [
+    (
+        "Tietokantasovellus",
+        """
+    Tietokantasovellus on tietojenkäsittelytieteen harjoitustyökurssi, jossa toteutetaan tietokantaa käyttävä web-sovellus. Kurssin laajuus on 4 op ja sen esitietokurssit ovat Ohjelmoinnin perusteet, Ohjelmoinnin jatkokurssi sekä Tietokantojen perusteet.
+
+    Kurssin tavoitteena on syventää taitoja tietokantojen käyttämisessä sekä myös opettaa web-ohjelmointia ja sovelluskehitystä. Sovellus toteutetaan Pythonilla Flask-kirjastolla käyttäen PostgreSQL-tietokantaa. Kurssilla käytetään myös GitHub- ja Fly.io-palveluja.
+    """,
+        "Tietokantasovellus on tietojenkäsittelytieteen harjoitustyökurssi, jossa toteutetaan tietokantaa käyttävä web-sovellus...",
+        "9ec7c2c2-9d75-406f-b22c-e4920707de0b",
+    ),
+    (
+        "Ohjelmistotekniikka",
+        """
+    Kurssilla opit tuntemaan ohjelmistotuotantoprosessin vaiheet. Kurssin jälkeen tunnet tärkeimpiä ohjelmiston suunnitteluperiaatteita ja osaat soveltaa niitä yksinkertaisissa projekteissa. Olet tietoinen vesiputousmallin ja ketterän ohjelmistotuotannon luonteesta.
+
+    Opit soveltamaan versionhallintaa osana ohjelmistokehitystä, sekä UML-mallinnustekniikkaa vaatimusmäärittelyssä ja ohjelmiston suunnittelussa tarkoituksenmukaisella tavalla.
+
+    Kurssin jälkeen tunnet ohjelmiston testauksen vaiheet ja osaat soveltaa automatisoitua testausta yksinkertaisissa ohjelmistoprojekteissa.
+    """,
+        "Kurssilla opit tuntemaan ohjelmistotuotantoprosessin vaiheet. Kurssin jälkeen tunnet tärkeimpiä ohjelmiston suunnittelu...",
+        "49c796ee-8056-4fe5-a4ee-b7898547b310",
+    ),
+    (
+        "TIRA 2",
+        """
+    Tietorakenteet ja algoritmit on tietojenkäsittelytieteen aineopintokurssi, joka muodostuu kahdesta 5 op laajuisesta osasuorituksesta. Kurssin aiheita ovat mm. järjestäminen, lista- ja joukkorakenteet, dynaaminen ohjelmointi sekä verkkoalgoritmit.
+
+    Keväällä 2023 voit suorittaa kurssin verkkokurssina, jonka materiaali on tällä sivustolla. Kurssi suoritetaan ratkomalla automaattisesti palautettavia tehtäviä. Kurssi järjestetään samalla tavalla Helsingin yliopiston tutkinto-opiskelijoille ja Avoimen yliopiston opiskelijoille.
+    """,
+        "Tietorakenteet ja algoritmit on tietojenkäsittelytieteen aineopintokurssi, joka muodostuu kahdesta 5 op laajuisesta osas...",
+        "7a00ffac-b1b8-406b-928f-f47ef80566fc",
+    ),
+]
