24a5e4965a3de27ba8368ca5728029490d701a6d
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-04-10 23:43:35 +0300

    remove unnecessary info from session

--- flaskr/modules/auth.py
+++ flaskr/modules/auth.py
@@ -1,6 +1,7 @@
 from modules.db import make_query, make_insert
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 # try to login using given credentials
 # returns success flag as boolean
 def try_login(email: str, password: str) -> bool:
@@ -49,7 +50,9 @@ def create_user(name: str, email: str, password: str, userType: str) -> str:
 # get user using email address
 def get_user_by_email(email: str):
     # query for user existence
-    query = make_query("SELECT * FROM users WHERE email = :email", {"email": email})
+    query = make_query(
+        "SELECT id, name, email, type FROM users WHERE email = :email", {"email": email}
+    )
     user = query.fetchone()
     return user
 
@@ -57,6 +60,8 @@ def get_user_by_email(email: str):
 # get user using uuid
 def get_user_by_id(id: str):
     # query for user existence
-    query = make_query("SELECT * FROM users WHERE id = :id", {"id": id})
+    query = make_query(
+        "SELECT id, name, email, type FROM users WHERE id = :id", {"id": id}
+    )
     user = query.fetchone()
     return user
