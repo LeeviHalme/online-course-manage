90ee02c52cab144005b2df6051349010f7faaa54
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-04-10 23:50:03 +0300

    add course enrolment :sparkles:

--- flaskr/modules/courses.py
+++ flaskr/modules/courses.py
@@ -1,4 +1,4 @@
-from modules.db import make_query
+from modules.db import make_query, make_insert, serialize_to_dict
 
 # get all courses from db
 def get_courses(hidden=False):
@@ -29,3 +29,52 @@ def find_by_id(course_id: str):
         "SELECT * FROM courses WHERE id = :course_id", {"course_id": course_id}
     )
     return query.fetchone()
+
+
+# check if user is a participant on a course
+# returns boolean status flag
+def is_enrolled(course_id: str, user_id: str):
+    params = {"course_id": course_id, "user_id": user_id}
+    query = make_query(
+        "SELECT 1 from participants WHERE user_id = :user_id AND course_id = :course_id",
+        params,
+    )
+    result = query.fetchone()
+
+    if result:
+        return True
+    else:
+        return False
+
+
+# validate invitation code
+# returns boolean status flag
+def validate_invitation_code(course_id: str, code: str):
+    result = find_by_id(course_id)
+
+    # if course_id was invalid
+    if not result:
+        return False
+
+    # if code was invalid
+    course = serialize_to_dict(result)
+
+    print(course["invitation_code"])
+
+    if course["invitation_code"] != code:
+        return False
+    else:
+        return True
+
+
+# create a new participant record
+def enroll_to_course(course_id: str, user_id: str):
+    # insert user into database
+    params = {
+        "user_id": user_id,
+        "course_id": course_id,
+    }
+    text_query = (
+        "INSERT INTO participants (user_id, course_id) VALUES (:user_id, :course_id)"
+    )
+    make_insert(text_query, params)
--- flaskr/routes/courses.py
+++ flaskr/routes/courses.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template, abort, request
-from modules.courses import get_courses, search_courses_by_name, find_by_id
+from flask import Blueprint, render_template, abort, request, redirect, session
+from modules.courses import (
+    get_courses,
+    search_courses_by_name,
+    find_by_id,
+    is_enrolled,
+    validate_invitation_code,
+    enroll_to_course,
+)
+from modules.db import serialize_to_dict
 
 courses = Blueprint("courses", __name__, url_prefix="/courses")
+
 
 # view all public courses page
 @courses.route("/", methods=["GET"])
@@ -23,7 +32,7 @@ def show_courses():
 
 
 # view course info page
-@courses.route("/<course_id>")
+@courses.route("/<course_id>", methods=["GET"])
 def view_course(course_id: str):
     course = find_by_id(course_id)
 
@@ -31,4 +40,52 @@ def view_course(course_id: str):
     if not course:
         return abort(404)
 
-    return render_template("view_course.html", course=course)
+    # check additional state passed
+    args = request.args.to_dict()
+    alert = None
+    if args.get("alreadyEnrolled"):
+        alert = ("danger", "You've already enrolled to this course!")
+    elif args.get("invalidCode"):
+        alert = ("danger", "Invalid invitation code! Please double-check spelling")
+
+    return render_template(
+        "view_course.html", course=serialize_to_dict(course), alert=alert
+    )
+
+
+# enroll to a course
+@courses.route("/<course_id>/join", methods=["POST"])
+def enroll(course_id: str):
+    # if user is not logged in
+    user = session["user"]
+    if not user:
+        return abort(401)
+
+    # get request body
+    code = request.form["invitation_code"]
+
+    # TODO: Validate request body
+
+    # get course from db
+    course = find_by_id(course_id)
+
+    # if course doesn't exist
+    if not course:
+        return abort(404)
+
+    enrolled = is_enrolled(course_id, user["id"])
+
+    # if user is already enrolled
+    if enrolled:
+        return redirect(f"/courses/{course_id}?alreadyEnrolled=true")
+
+    valid = validate_invitation_code(course_id, code)
+
+    # if invite code was not valid
+    if not valid:
+        return redirect(f"/courses/{course_id}?invalidCode=true")
+
+    # create new participant record
+    enroll_to_course(course_id, user["id"])
+
+    return redirect(f"/courses/{course_id}")
