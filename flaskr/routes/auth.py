257e7f9be5d880ed1413c7490a91a0f4517f487e
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-04-26 23:12:16 +0300

    add validation (correct commit)

--- flaskr/routes/auth.py
+++ flaskr/routes/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session, flash
 from modules.auth import try_login, get_user_by_email, create_user
 from modules.db import serialize_to_dict
+import utils.validators as validate
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -14,6 +15,11 @@ def login():
     # get request body
     email = request.form["email"]
     password = request.form["password"]
+
+    # Validate email
+    if not validate.email(email):
+        flash("Malformed email address!", "danger")
+        return redirect(request.url)
 
     # try logging in
     success = try_login(email, password)
@@ -37,18 +43,37 @@ def register():
         return render_template("register.html")
 
     # get request body
-    name = request.form["name"]
-    email = request.form["email"]
-    userType = request.form["userType"]
-    password = request.form["password"]
-    passwordConfirm = request.form["passwordConfirm"]
+    values = request.form
+    name = values.get("name")
+    email = values.get("email")
+    user_type = values.get("userType")
+    password = values.get("password")
+    password_confirm = values.get("passwordConfirm")
 
-    # check if passwords won't match
-    if password != passwordConfirm:
-        flash("Passwords won't match!", "danger")
+    # Validate name (alphabetic, 5-50 chars)
+    if len(name) < 5 or len(name) > 50:
+        flash("Invalid name! 5-50 characters.", "danger")
         return redirect(request.url)
 
-    # TODO: Add validation
+    # Validate email
+    elif not validate.email(email):
+        flash("Invalid email address!", "danger")
+        return redirect(request.url)
+
+    # Validate userType (either 'TEACHER' or 'STUDENT')
+    elif not validate.user_type(user_type):
+        flash("Please choose your user type!", "danger")
+        return redirect(request.url)
+
+    # validate password (alphanumeric and >= 5 characters)
+    elif not validate.password(password):
+        flash("Invalid password! At least 5 characters (a-Z, 0-9)", "danger")
+        return redirect(request.url)
+
+    # check if passwords won't match
+    elif password != password_confirm:
+        flash("Passwords won't match!", "danger")
+        return redirect(request.url)
 
     # check if user already exists
     user = get_user_by_email(email)
@@ -57,7 +82,7 @@ def register():
         return redirect(request.url)
 
     # create new user
-    create_user(name, email, password, userType)
+    create_user(name, email, password, user_type)
 
     # Add session
     user = get_user_by_email(email)
