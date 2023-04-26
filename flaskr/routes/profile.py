c3311b2c9453727c0b0bb884fb1f12060cab055f
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-04-26 23:06:09 +0300

    add first validators

--- flaskr/routes/profile.py
+++ flaskr/routes/profile.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, abort, session
-from modules.profile import get_enrolled_courses
+from flask import Blueprint, render_template, abort, redirect, session, request, flash
+from modules.profile import get_enrolled_courses, validate_email_list
+from modules.courses import create_course
 from datetime import datetime
+import utils.validators as validate
 
 profile = Blueprint("profile", __name__, url_prefix="/profile")
 
@@ -31,7 +33,7 @@ def dashboard():
 
 # create course route
 @profile.route("/dashboard/create-course", methods=["GET", "POST"])
-def create_course():
+def create_course_route():
     # if user is not logged in
     user = session.get("user")
     if not user:
@@ -41,4 +43,68 @@ def create_course():
     if user["type"] != "TEACHER":
         return abort(403)
 
-    return render_template("teacher/create_course.html")
+    # if this was GET request, return the form
+    if request.method == "GET":
+        return render_template("teacher/create_course.html")
+
+    # get request body
+    values = request.form
+    name = values.get("name")
+    short_description = values.get("short_description")
+    description = values.get("description")
+    invitation_code = values.get("invitation_code")
+    course_teachers = values.get("course_teachers[]")
+    is_hidden = values.get("is_hidden")
+    is_public = values.get("is_public")
+
+    # validate name (5-120 chars)
+    if len(name) < 5 or len(name) > 120:
+        flash("Invalid name! Must be between 5-120 chars", "danger")
+        return redirect(request.url)
+
+    # validate short description (0-120 chars)
+    elif len(short_description) > 120:
+        flash("Invalid short description! Max 120 chars")
+        return redirect(request.url)
+
+    # validate description (string)
+    elif type(description) != str:
+        flash("Invalid description!")
+        return redirect(request.url)
+
+    # validate invitation code (alphanumeric, max 15 chars)
+    elif not validate.alpha(invitation_code) or len(invitation_code) > 15:
+        flash("Invalid invitation code! Max 15 characters (a-Z, 0-9)", "danger")
+        return redirect(request.url)
+
+    # validate is_hidden checkbox
+    elif is_hidden and not validate.boolean(is_hidden):
+        flash("Invalid is_hidden flag value", "danger")
+        return redirect(request.url)
+
+    # validate is_public checkbox
+    elif is_public and not validate.boolean(is_public):
+        flash("Invalid is_public flag value", "danger")
+        return redirect(request.url)
+
+    # validate teachers email array
+    teacher_list = course_teachers.split(",")
+    valid = validate_email_list("TEACHER", teacher_list)
+
+    # if email list was not valid
+    if not valid:
+        flash(
+            "Invalid teachers! Make sure you entered registered teachers email's only.",
+            "danger",
+        )
+        return redirect(request.url)
+
+    # create new course
+    create_course(
+        name, short_description, description, invitation_code, is_hidden, is_public
+    )
+
+    # show success msg
+    flash("Successfully created new course!", "success")
+
+    return redirect("/profile/dashboard")
---
+++ flaskr/utils/validators.py
@@ -0,0 +1,39 @@
+# Collection of simple validators
+import re
+
+
+# Validate E-Mail addresses
+# Source: https://stackabuse.com/python-validate-email-address-with-regular-expressions-regex/
+def email(input: str) -> bool:
+    regex = re.compile(
+        r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+"
+    )
+    return bool(re.fullmatch(regex, input))
+
+
+# Validate booleans
+def boolean(input: str) -> bool:
+    if input == "on":
+        return True
+    else:
+        return False
+
+
+# Validate alphabetic strings
+def alpha(input: str) -> bool:
+    return input.isalpha()
+
+
+# Validate password (alphanumeric and >= 5 characters)
+def password(input: str) -> bool:
+    return alpha(input) and len(input) >= 5
+
+
+# Validate SQL USER_TYPE
+def user_type(input: str):
+    if input == "STUDENT":
+        return True
+    elif input == "TEACHER":
+        return True
+    else:
+        return False
